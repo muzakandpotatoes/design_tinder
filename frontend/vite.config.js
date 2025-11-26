baeede9a2b7b378286cff2cbc5f857bb1b40c5b3
muzakandpotatoes <muzakandpotatoes@proton.me>
2025-11-25 16:53:21 -0800

    Set base path for Vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/design_tinder/',
   server: {
     proxy: {
       '/api': 'http://localhost:8000',
